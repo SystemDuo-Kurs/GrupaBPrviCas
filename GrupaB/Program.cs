b0311ad767bcdfe710516771524e9f48e3c6f7fd
Dragan Pepic <d.pepic@hotmail.com>
2021-08-09 19:56:20 +0200

    Add project files.

---
+++ GrupaB/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace GrupaB
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int broj = 27;
+
+			while (broj > 1)
+			{
+				Console.WriteLine(broj);
+				if (broj % 2 == 0)
+				{
+					broj = broj / 2;
+				} else
+				{
+					broj = broj * 3 + 1;
+				}	
+			}
+			Console.WriteLine(broj);
+		}
+	}
+}
